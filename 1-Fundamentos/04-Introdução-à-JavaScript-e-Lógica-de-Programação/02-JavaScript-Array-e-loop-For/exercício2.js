6e0f000a8e4273dbaed98d0894f68f2d20a7df9b
ArthurOliveiraSou <arthurnl.oliveira@gmail.com>
2022-05-18 19:54:51 -0300

    adiciona os exercícios do dia 4.2, incluindo o bônus

---
+++ 1-Fundamentos/04-Introdução-à-JavaScript-e-Lógica-de-Programação/02-JavaScript-Array-e-loop-For/exercício2.js
@@ -0,0 +1,7 @@
+let array = [];
+
+for(let index = 0; index == array.length && array.length < 25; index += 1) {
+ array.push(index + 1);
+ console.log(array[index]);
+ console.log(array[index] / 2);
+}
